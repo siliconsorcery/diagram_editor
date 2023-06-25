4964fa2b5794f694f306db3a8ac40c14ed2e3900
kicu-xxo <kicu.xxo@gmail.com>
2023-06-25 23:14:38 +0900

    move midPoint 1, 2 수정

--- lib/src/canvas_context/canvas_model.dart
+++ lib/src/canvas_context/canvas_model.dart
@@ -144,13 +144,19 @@ class CanvasModel with ChangeNotifier {
     var xPosition = (sourcePoint.dx - targetPoint.dx).abs();
     var yPosition = (sourcePoint.dy - targetPoint.dy).abs();
 
-    Offset midPoint1 = xPosition > yPosition
-        ? Offset((sourcePoint.dx + targetPoint.dx) / 2, sourcePoint.dy)
-        : Offset(sourcePoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
+    // Offset midPoint1 = xPosition > yPosition
+    //     ? Offset((sourcePoint.dx + targetPoint.dx) / 2, sourcePoint.dy)
+    //     : Offset(sourcePoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
 
-    Offset midPoint2 = xPosition > yPosition
-        ? Offset((sourcePoint.dx + targetPoint.dx) / 2, targetPoint.dy)
-        : Offset(targetPoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
+    // Offset midPoint2 = xPosition > yPosition
+    //     ? Offset((sourcePoint.dx + targetPoint.dx) / 2, targetPoint.dy)
+    //     : Offset(targetPoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
+
+    Offset midPoint1 =
+        Offset(sourcePoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
+
+    Offset midPoint2 =
+        Offset(targetPoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
 
     linkPoints[1] = midPoint1;
     linkPoints[2] = midPoint2;
