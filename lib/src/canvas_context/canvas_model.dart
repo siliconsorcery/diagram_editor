71babda3d288b3e90995c43b31aa7e1abb6d39e2
kicu <kicu.xxo@gmail.com>
2023-06-26 09:42:45 +0900

    midPoint 1, 2 test

--- lib/src/canvas_context/canvas_model.dart
+++ lib/src/canvas_context/canvas_model.dart
@@ -144,11 +144,11 @@ class CanvasModel with ChangeNotifier {
     var xPosition = (sourcePoint.dx - targetPoint.dx).abs();
     var yPosition = (sourcePoint.dy - targetPoint.dy).abs();
 
-    Offset midPoint1 = ((xPosition - yPosition).abs() < 10)
+    Offset midPoint1 = ((xPosition - yPosition).abs() < 20)
         ? Offset((sourcePoint.dx + targetPoint.dx) / 2, sourcePoint.dy)
         : Offset(sourcePoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
 
-    Offset midPoint2 = ((xPosition - yPosition).abs() < 10)
+    Offset midPoint2 = ((xPosition - yPosition).abs() < 20)
         ? Offset((sourcePoint.dx + targetPoint.dx) / 2, targetPoint.dy)
         : Offset(targetPoint.dx, (sourcePoint.dy + targetPoint.dy) / 2);
 
